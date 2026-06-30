714231d8c0b7c5518eecfc6c83f40f87eba4acd5
Nikhil Barad <nikhilbarad465@gmail.com>
2026-06-30 11:35:31 +0530

    Added DTO Folder and DTO file

---
+++ StudentProj.Application/DTO/AssignPermissionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentProj.Application.DTO
+{
+    public class AssignPermissionDTO
+    {
+        public int RoleId { get; set; }
+        public string PermissionIds { get; set; }
+        public int MenuId { get; set; }
+    }
+}
---
+++ StudentProj.Application/DTO/PermissionDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProj.Application.DTO
+{
+    public class PermissionDTO
+    {
+        public string PermissionName { get; set; }
+    }
+}
