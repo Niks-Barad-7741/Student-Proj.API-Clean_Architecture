92082858f6b62f509f594dbddd80971394b3fc74
Nikhil Barad <nikhilbarad465@gmail.com>
2026-06-30 18:18:05 +0530

    Added HttpContext NuGetPackege which was not earlier

--- StudentProj.Core/Common/IpHelper.cs
+++ StudentProj.Core/Common/IpHelper.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Http;
 using System.Net;
-using System.Net.Http;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudentProj.Core.Common
 {
